b25e7ed20c85933660145f82252824a00bc07af0
azhararrozak <azhardrozak2001@upi.edu>
2024-02-21 10:48:39 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+
+//import packages
+import * as ImagePicker from 'expo-image-picker';
+
+// import components
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+// location images
+const imageLocation = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled){
+      // console.log(result)
+      setSelectedImage(result.assets[0].uri);
+    }else{
+      alert('Nothing selected');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer selectedImage={selectedImage} imageLocation={imageLocation} />
+      </View>
+      <View style={styles.footerContainer} >
+        <Button theme="primary" label="Choose your photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  }
 });
